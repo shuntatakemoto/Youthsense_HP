a493ec2c20923f5fea5e47a24a538efaf7f1112f
shuntatakemoto <rt80405@gmail.com>
2020-11-09 23:56:54 +0900

    pc版のtop進めました

--- public/assets/js/back.js
+++ public/assets/js/back.js
@@ -4,9 +4,9 @@ $(function () {
 
 function rect() {
   $("#rect").animate({
-      top: "-500px" //要素を動かす位置
-  }, 8000).animate({
-      top: "500px" //要素を戻す位置
+      top: "-3000px" //要素を動かす位置
+  }, 30000).animate({
+      top: "3000px" //要素を戻す位置
   }, 0)
-  setTimeout("rect()", 8000);//アニメーションを繰り返す間隔
+  setTimeout("rect()", 30000);//アニメーションを繰り返す間隔
 }
